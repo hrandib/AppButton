100e0ea0d10a7258a200f9afca4ba215d3eb47bc
Dmytro Shestakov <dmytro.shestakov@globallogic.com>
2018-12-24 11:54:19 +0200

    Add binary

---
+++ bin/run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#To run this script socat application should be existed in the source system
+
+trap "kill 0" EXIT
+MASTER_PORT=./tty_master
+socat PTY,link=$MASTER_PORT,raw,echo=0 PTY,link=./tty_slave,raw,echo=0,crnl &
+MASTER_PORT_ABS=$(readlink -f $MASTER_PORT)
+echo $MASTER_PORT_ABS
+java -jar AppButton.jar $MASTER_PORT_ABS
+wait
